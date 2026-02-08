8564cabcfadd4b56531d1daf273a9d462c142014
Abhiraj212024 <abhirajraje1@gmail.com>
2026-02-08 16:42:08 +0530

    COmpleted Cellular Network with Two Pointers O(n)

--- Silver/Two_Pointers/Cellular_Network/main.cpp
+++ Silver/Two_Pointers/Cellular_Network/main.cpp
@@ -14,7 +14,8 @@ int main(){
     for(int i{}; i < m; i++) cin >> b[i];
 
     int max_r = INT_MIN;
-    for(int i{}; i < n; i++){
+    // Binary Search Approach (nlogn)
+ /*   for(int i{}; i < n; i++){
         int tower_right = lower_bound(b.begin(), b.end(), a[i]) - b.begin();
         int tower_left = tower_right - 1;
         int min_dist = INT_MAX;
@@ -26,7 +27,24 @@ int main(){
         }
         max_r = max(max_r, min_dist);
     }
+*/
 
+    // Two Pointers Approach (n)
+    
+    vector<int> dist(n, 0);
+    int j = 0;
+    for(int i = 0; i < n; i++){
+        while(j+1 < m && b[j+1] <= a[i]) j++;
+        dist[i] = abs(a[i] - b[j]);
+    }
+
+    j = m-1;
+    for(int i = n-1; i >= 0; i--){
+        while(j-1>=0 && b[j-1] >= a[i]) j--;
+        dist[i] = min(dist[i], abs(b[j] - a[i]));
+    }
+
+    for(int i = 0; i < n; i++) max_r = max(max_r, dist[i]);
     cout << max_r << "\n";
     return 0;
 }
