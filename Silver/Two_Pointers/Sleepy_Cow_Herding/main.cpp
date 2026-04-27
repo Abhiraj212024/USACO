bb39bc91e6641474e2496d53a9840739806ed4b8
Abhiraj212024 <abhirajraje1@gmail.com>
2026-04-27 18:42:47 +0530

    completed Sleepy Cow Herdingt

---
+++ Silver/Two_Pointers/Sleepy_Cow_Herding/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+    int N;
+    cin >> N;
+
+    vector<int> a(N);
+    for(int i{}; i < N; i++) cin >> a[i];
+
+    
+
+    return 0;
+}
