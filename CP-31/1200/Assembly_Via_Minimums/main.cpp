25aa6241ca0a5b050606740c0d8d62ab2e995982
Abhiraj212024 <abhirajraje1@gmail.com>
2026-06-08 23:43:38 +0530

    completed Assembly_Via_Minimums

---
+++ CP-31/1200/Assembly_Via_Minimums/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin >> n;
+        int l = (n * (n-1))/2;
+        vector<int> b(l);
+        for(int i = 0;i < l; i++) cin >> b[i];
+        sort(b.begin(), b.end());
+        vector<int> ans;
+        int j = 0;
+        while(n > 1 && j < l){
+            ans.push_back(b[j]);
+            j += n-1;
+            n--;
+        }
+        ans.push_back(b[l-1] + 1);
+
+        for(const int& num : ans){
+            cout << num << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
