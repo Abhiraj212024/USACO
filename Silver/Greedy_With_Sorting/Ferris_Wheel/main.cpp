429cd78a762dc144f4fb6ce5319bc75ac85daa30
Abhiraj212024 <abhirajraje1@gmail.com>
2026-06-11 00:14:09 +0530

    completed ferris wheel

---
+++ Silver/Greedy_With_Sorting/Ferris_Wheel/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+    int n, x;
+    cin >> n >> x;
+    vector<int> p(n);
+
+    for(int i = 0; i < n; i++) cin >> p[i];
+
+    sort(p.begin(), p.end());
+
+    int i = 0;
+    int j = n-1;
+    int count = 0;
+    while(i <= j){
+        if(p[i] + p[j] <= x){
+            i++;
+            j--;
+        }else{
+            j--;
+        }
+        count++;
+    }
+
+    cout << count << "\n";
+
+    return 0;
+}
