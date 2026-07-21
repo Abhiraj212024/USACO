38b252f5def771b71b138b96286be89e885bdd9b
Abhiraj212024 <abhirajraje1@gmail.com>
2026-07-21 15:22:15 +0530

    modified stuff

--- Gold/Knapsack_DP/Book_Shop/main.cpp
+++ Gold/Knapsack_DP/Book_Shop/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <climits>
-
+#include <algorithm>
 using namespace std;
 
 int main(){
@@ -11,18 +11,26 @@ int main(){
     for(int i = 1; i <= n; i++) cin >> h[i];
     vector<int> s(n+1, 0); // pages
     for(int i = 1; i <= n; i++) cin >> s[i];
-    
-    vector<vector<int>> dp(n+1, vector<int>(x+1, 0));
-
+    vector<pair<int, int>> v;
     for(int i = 1; i <= n; i++){
-        for(int j = 1; j <= x; j++){
-            dp[i][j] = dp[i-1][j];
-            if(j - h[i] < 0) continue;
-            dp[i][j] = max(dp[i-1][j], dp[i-1][j - h[i]] + s[i]);
+        v.push_back({h[i], s[i]});
+    }
+    sort(v.begin(), v.end());
+    vector<int> dp(x+1, INT_MIN); // dp[i] -> maximum number of pages for price i
+    dp[0] = 0;
+    for(const pair<int, int>& p : v){
+        int price = p.first;
+        int pages = p.second;
+
+        for(int j = x; j >= 1; j--){
+            if(j - price < 0) continue;
+            if(dp[j-price] != INT_MIN){
+                dp[j] = max(dp[j], dp[j-price] + pages);
+            }
         }
     }
 
-    cout << dp[n][x] << "\n";
+    cout << *max_element(dp.begin(), dp.end()) << "\n";
 
     return 0;
 }
